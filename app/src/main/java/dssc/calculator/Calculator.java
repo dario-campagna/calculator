c2b41bcd440db13cbcf8a543046e098d703f4641
Dario Campagna <campagna@esteco.com>
2022-10-21 17:21:11 +0200

    Remove duplication

--- app/src/main/java/dssc/calculator/Calculator.java
+++ app/src/main/java/dssc/calculator/Calculator.java
@@ -2,11 +2,6 @@ package dssc.calculator;
 
 public class Calculator {
     public double compute(String s) {
-        if ("100".equals(s)) {
-            return 100;
-        } else if ("99.99".equals(s)) {
-            return 99.99;
-        }
-        return -3.14;
+        return Double.parseDouble(s);
     }
 }
