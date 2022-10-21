de5d39580999c0969c35f3fb641bf215a67f069e
Dario Campagna <campagna@esteco.com>
2022-10-21 18:23:00 +0200

    Remove duplication

--- app/src/main/java/dssc/calculator/Calculator.java
+++ app/src/main/java/dssc/calculator/Calculator.java
@@ -1,18 +1,26 @@
 package dssc.calculator;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Calculator {
+
+    private static Map<String, BiFunction<Double, Double, Double>> operatorToFunction = new HashMap<>(){{
+        put("+", (l, r) -> l + r);
+        put("*", (l, r) -> l * r);
+        put("-", (l, r) -> l - r);
+        put("/", (l, r) -> l / r);
+    }};
+
     public double compute(String s) {
         return Double.parseDouble(s);
     }
 
     public double compute(String leftOperand, String operator, String rightOperand) {
-        if ("*".equals(operator)) {
-            return Double.parseDouble(leftOperand) * Double.parseDouble(rightOperand);
-        } else if ("/".equals(operator)) {
-            return Double.parseDouble(leftOperand) / Double.parseDouble(rightOperand);
-        } else if ("-".equals(operator)) {
-            return Double.parseDouble(leftOperand) - Double.parseDouble(rightOperand);
-        }
-        return Double.parseDouble(leftOperand) + Double.parseDouble(rightOperand);
+        double leftOp = Double.parseDouble(leftOperand);
+        double rightOp = Double.parseDouble(rightOperand);
+        return operatorToFunction.get(operator).apply(leftOp, rightOp);
     }
 }
