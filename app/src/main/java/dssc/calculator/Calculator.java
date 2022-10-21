49169811f5cd66c7e4a1205602d40e0c171f1f72
Dario Campagna <campagna@esteco.com>
2022-10-21 17:40:02 +0200

    We now compute 1+2

--- app/src/main/java/dssc/calculator/Calculator.java
+++ app/src/main/java/dssc/calculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public double compute(String s) {
         return Double.parseDouble(s);
     }
+
+    public int compute(String leftOperand, String operator, String rightOperand) {
+        return 3;
+    }
 }
--- app/src/test/java/ComputeResultFor.java
+++ app/src/test/java/ComputeResultFor.java
@@ -1,4 +1,5 @@
 import dssc.calculator.Calculator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,5 +15,10 @@ public class ComputeResultFor {
     })
     void just_a_number(double result, String number) {
         assertEquals(result, new Calculator().compute(number));
+    }
+
+    @Test
+    void two_plus_one() {
+        assertEquals(3, new Calculator().compute("2", "+", "1"));
     }
 }
