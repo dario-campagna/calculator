07980d749aa6f5d7738c230b43a774029f34783c
Dario Campagna <campagna@esteco.com>
2022-10-21 19:39:03 +0200

    Switch case to avoid unecessary complexity

--- app/src/main/java/dssc/calculator/Calculator.java
+++ app/src/main/java/dssc/calculator/Calculator.java
@@ -1,26 +1,26 @@
 package dssc.calculator;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-
 public class Calculator {
 
-    private static Map<String, BiFunction<Double, Double, Double>> operatorToFunction = new HashMap<>(){{
-        put("+", (l, r) -> l + r);
-        put("*", (l, r) -> l * r);
-        put("-", (l, r) -> l - r);
-        put("/", (l, r) -> l / r);
-    }};
-
-    public double compute(String s) {
-        return Double.parseDouble(s);
-    }
-
     public double compute(String... args) {
-        double leftOperand = Double.parseDouble(args[0]);
-        double rightOperand = Double.parseDouble(args[2]);
-        return operatorToFunction.get(args[1]).apply(leftOperand, rightOperand);
+        if (operatorIsPresent(args)) {
+            return applyOperation(Double.parseDouble(args[0]), args[1], Double.parseDouble(args[2]));
+        } else {
+            return Double.parseDouble(args[0]);
+        }
     }
+
+    private static boolean operatorIsPresent(String[] args) {
+        return args.length > 1;
+    }
+
+    private static Double applyOperation(double leftOperand, String operator, double rightOperand) {
+        return switch (operator) {
+            case "+" -> leftOperand + rightOperand;
+            case "*" -> leftOperand * rightOperand;
+            case "-" -> leftOperand - rightOperand;
+            default -> leftOperand / rightOperand;
+        };
+    }
+
 }
--- app/src/test/java/ComputeResultFor.java
+++ app/src/test/java/ComputeResultFor.java
@@ -1,11 +1,8 @@
 import dssc.calculator.Calculator;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,30 +11,23 @@ public class ComputeResultFor {
 
     private final Calculator calculator = new Calculator();
 
-    @ParameterizedTest
-    @CsvSource({
-            "-3.14, -3.14",
-            "100, 100",
-            "99.99, 99.99"
-    })
-    void just_a_number(double result, String number) {
-        assertEquals(result, calculator.compute(number));
-    }
-
     private static Stream<Arguments> argumentsProvider() {
         return Stream.of(
-            Arguments.arguments(new String[]{"2", "+", "1"}, 3),
-            Arguments.arguments(new String[]{"-3", "+", "3.14"}, 0.14),
-            Arguments.arguments(new String[]{"9", "*", "8.5"}, 76.5),
-            Arguments.arguments(new String[]{"-4", "*", "0"}, -0.0),
-            Arguments.arguments(new String[]{"4", "/", "2"}, 2),
-            Arguments.arguments(new String[]{"10.1", "-", "10"}, 0.1)
+                Arguments.arguments(new String[]{"3.14"}, 3.14),
+                Arguments.arguments(new String[]{"100"}, 100),
+                Arguments.arguments(new String[]{"99.99"}, 99.99),
+                Arguments.arguments(new String[]{"2", "+", "1"}, 3),
+                Arguments.arguments(new String[]{"-3", "+", "3.14"}, 0.14),
+                Arguments.arguments(new String[]{"9", "*", "8.5"}, 76.5),
+                Arguments.arguments(new String[]{"-4", "*", "0"}, -0.0),
+                Arguments.arguments(new String[]{"4", "/", "2"}, 2),
+                Arguments.arguments(new String[]{"10.1", "-", "10"}, 0.1)
         );
     }
 
     @ParameterizedTest
     @MethodSource("argumentsProvider")
-    void single_operation_args(String[] args, double result) {
+    void single_operation(String[] args, double result) {
         assertEquals(result, calculator.compute(args), 0.000001);
     }
 
