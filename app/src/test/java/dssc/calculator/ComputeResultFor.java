06d0e91df1040b8e6247fef36bc51389474be1a0
Dario Campagna <campagna@esteco.com>
2022-10-21 19:59:50 +0200

    Refactor tests

--- app/src/test/java/dssc/calculator/ComputeResultFor.java
+++ app/src/test/java/dssc/calculator/ComputeResultFor.java
@@ -2,6 +2,7 @@ package dssc.calculator;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -12,11 +13,24 @@ public class ComputeResultFor {
 
     private final Calculator calculator = new Calculator();
 
-    private static Stream<Arguments> argumentsProvider() {
+    @ParameterizedTest
+    @CsvSource({
+            "3.14, 3.14",
+            "100, 100",
+            "99.99, 99.99"
+    })
+    void single_number(String number, double result) {
+        assertEquals(result, calculator.compute(number), 0.000001);
+    }
+
+    @ParameterizedTest
+    @MethodSource("singleOperations")
+    void single_operation(String[] args, double result) {
+        assertEquals(result, calculator.compute(args), 0.000001);
+    }
+
+    private static Stream<Arguments> singleOperations() {
         return Stream.of(
-                Arguments.arguments(new String[]{"3.14"}, 3.14),
-                Arguments.arguments(new String[]{"100"}, 100),
-                Arguments.arguments(new String[]{"99.99"}, 99.99),
                 Arguments.arguments(new String[]{"2", "+", "1"}, 3),
                 Arguments.arguments(new String[]{"-3", "+", "3.14"}, 0.14),
                 Arguments.arguments(new String[]{"9", "*", "8.5"}, 76.5),
@@ -24,12 +38,6 @@ public class ComputeResultFor {
                 Arguments.arguments(new String[]{"4", "/", "2"}, 2),
                 Arguments.arguments(new String[]{"10.1", "-", "10"}, 0.1)
         );
-    }
-
-    @ParameterizedTest
-    @MethodSource("argumentsProvider")
-    void single_operation(String[] args, double result) {
-        assertEquals(result, calculator.compute(args), 0.000001);
     }
 
 }
