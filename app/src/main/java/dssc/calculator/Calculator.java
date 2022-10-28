dce460ed16046f42e6bbbf5615958643a2e88644
Dario Campagna <campagna@esteco.com>
2022-10-28 09:55:08 +0200

    We now manage three different single numbers as input

--- app/src/main/java/dssc/calculator/Calculator.java
+++ app/src/main/java/dssc/calculator/Calculator.java
@@ -4,6 +4,8 @@ public class Calculator {
     public double compute(String number) {
         if ("100.93".equals(number)) {
             return 100.93;
+        } else if ("0.1".equals(number)) {
+            return 0.1;
         }
         return -3.14;
     }
--- app/src/test/java/dssc/calculator/CalculatorReturns.java
+++ app/src/test/java/dssc/calculator/CalculatorReturns.java
@@ -2,6 +2,8 @@ package dssc.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorReturns {
@@ -21,5 +23,11 @@ class CalculatorReturns {
     void another_number() {
         Calculator calculator = new Calculator();
         assertEquals(100.93, calculator.compute("100.93"), 0.000001);
+    }
+
+    @Test
+    void yet_another_number() {
+        Calculator calculator = new Calculator();
+        assertEquals(0.1, calculator.compute("0.1"), 0.000001);
     }
 }
