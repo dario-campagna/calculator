54442fac2e31c34cf16cd0fdb126425d2f34b8c7
Dario Campagna <campagna@esteco.com>
2022-10-28 12:07:59 +0200

    We now print result of operation

--- app/src/main/java/dssc/calculator/Main.java
+++ app/src/main/java/dssc/calculator/Main.java
@@ -3,6 +3,10 @@ package dssc.calculator;
 public class Main {
     public static void main(String... args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.compute(args[0]));;
+        if (args.length > 1) {
+            System.out.println(calculator.compute(args[0], args[1], args[2]));
+        } else {
+            System.out.println(calculator.compute(args[0]));
+        }
     }
 }
--- app/src/test/java/dssc/calculator/PrintOnConsole.java
+++ app/src/test/java/dssc/calculator/PrintOnConsole.java
@@ -10,12 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PrintOnConsole {
 
     @Test
-    void result_of_computation() {
+    void result_of_computation_for_single_number() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
         Main.main("42");
 
         assertEquals("42.0\n", outputStream.toString());
+    }
+
+    @Test
+    void result_of_computation_for_operation() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        Main.main("10.1","+","1");
+
+        assertEquals("11.1\n", outputStream.toString());
     }
 }
